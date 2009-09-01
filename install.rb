fb1fc419d60bb74f4b32f350c7633efd9d33274d
grosser <grosser.michael@gmail.com>
2009-09-01 10:25:38 +0200

    install: without backup = simpler, gitconfig: remote is not a color

--- install.rb
+++ install.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 # -> run me with `ruby dotfiles/setup.rb`
 
-no_backup = ENV['NO_BACKUP']
-
 raise "you should checkout dotfiles to ~/dotfiles and got to ~/ !" unless File.exist?("dotfiles")
 
 #replace files through links
@@ -12,37 +10,23 @@ raise "you should checkout dotfiles to ~/dotfiles and got to ~/ !" unless File.e
   '.irbrc'=>'irbrc',
   '.ssh/config'=>'ssh/config'
 }.each do |original,replaced|
-  if File.exist?(original)
-    if no_backup
-      `rm -f #{original}`
-    else
-      `mv #{original} #{original}.backup`
-    end
-  end
+  `rm -f #{original}`
   `ln -s ~/dotfiles/#{replaced} #{original}`
 end
 
 #replace folder through links
 folder = 'bin'
-if File.exist?(folder)
-  if no_backup
-    `rm -rf #{folder}`
-  else
-    `mv #{folder} #{folder}.backup`
-  end
-end
+`rm -rf #{folder}`
 `ln -s ~/dotfiles/#{folder} #{folder}`
 
 #do not share credentials + can always run install.rb
 # --> replace non-secret lines
 if File.exist?(".gitconfig")
-  `cp .gitconfig .gitconfig.backup` unless no_backup
-
   #extract github+user info from .gitconfig then combine with info stored here
   copy = false
   config = File.read('.gitconfig').split("\n")
   copied = config.map do |line|
-    copy = false if line =~ /\[[a-z]+\]/
+    copy = false if line =~ /^\[/
     copy = true if "[github] [user]".include? line.strip
     line if copy
   end.compact.reject{|x| x.empty?}
