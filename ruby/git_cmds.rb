45af438b80dcb3f6527e302c1c41ba3975a920fb
grosser <grosser.michael@gmail.com>
2010-03-13 10:30:19 +0100

    convert ruby aliases to ls-files + xarg calls

--- ruby/git_cmds.rb
+++ ruby/git_cmds.rb
@@ -1,15 +1,4 @@
-# variouse git comands that are stored here, so they can take advantage of ARGV
-# which normal git alias does not have
-# and because I dont like unix functions ...
-
-# perform some operation on matching files from `git st`
-def git_perform_on_st(find, execute)
-  st = `git st`
-  lines = st.split("\n").select{|x|x=~/#{find}/}
-  files = lines.map{|x|x.split(' ')[2]}
-  files.each{|f| eval("`#{execute.gsub('#F', f)}`")}
-end
-
+# can take advantage of ARGV, which git aliases cannot
 # check in all changes with a message tat does not need quotes around it <-> lazy
 def args_to_string(cmd)
   `#{cmd} "#{ARGV.join(' ').gsub('"','`')}"`
