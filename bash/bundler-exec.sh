7dd63827a330659e1aa96350512afbfbb2ab404e
grosser <grosser.michael@gmail.com>
2010-10-13 12:11:04 +0200

    add bundler-exec to never forget bundle exec again

---
+++ bash/bundler-exec.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BUNDLED_COMMANDS="cucumber rackup rails rake ruby shotgun spec"
+
+## Functions
+
+bundler-installed()
+{
+    which bundle > /dev/null 2>&1
+}
+
+within-bundled-project()
+{
+    local dir="$(pwd)"
+    while [ "$(dirname $dir)" != "/" ]; do
+        [ -f "$dir/Gemfile" ] && return
+        dir="$(dirname $dir)"
+    done
+    false
+}
+
+run-with-bundler()
+{
+    local command="$1"
+    shift
+    if bundler-installed && within-bundled-project; then
+        bundle exec $command "$@"
+    else
+        $command "$@"
+    fi
+}
+
+## Main program
+
+for CMD in $BUNDLED_COMMANDS; do
+    alias $CMD="run-with-bundler $CMD"
+done
--- bashrc
+++ bashrc
@@ -4,3 +4,4 @@ source ~/dotfiles/bash/settings
 source ~/dotfiles/bash/misc
 source ~/dotfiles/bash/git_prompt
 source ~/dotfiles/bash/rake_autocomplete
+source ~/dotfiles/bash/bundler-exec.sh
