5c7e59798036ad01f4d358a956e4b6b56435193a
grosser <grosser.michael@gmail.com>
2011-05-01 18:37:23 +0200

    move dawanda stuff into namespace

--- bash/personal.sh
+++ bash/personal.sh
@@ -2,7 +2,7 @@
 #Stuff that no-one else need, add and modify here to keep forks clean
 alias kj="pkill -f java"
 alias kf="pkill -f firefox"
-alias da="cd /apps/dawanda"
+alias da="cd /apps/dawanda/main"
 alias rs="cd /apps/rs"
 alias r="rvm 1.9.2 exec ruco"
 alias m="mine . > /dev/null 2>&1 &"
