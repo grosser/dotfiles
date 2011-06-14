69f62a05799cce9d638ac846100b7b25ea364753
grosser <grosser.michael@gmail.com>
2011-06-14 19:22:02 +0200

    fix wrake again

--- bash/personal.sh
+++ bash/personal.sh
@@ -6,7 +6,7 @@ alias da="cd /apps/dawanda/main"
 alias rs="cd /apps/rs"
 alias r="rvm 1.9.2 exec ruco"
 alias m="mine . > /dev/null 2>&1 &"
-alias wrake="`which rake`"
+alias wrake="`which rake` _0.8.7_"
 alias video-skype="LD_PRELOAD=/usr/lib/libv4l/v4l1compat.so skype"
 
 export GEMEDITOR='mine'
