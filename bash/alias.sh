56abef4377d53619920c46b578e184b03ec13285
grosser <grosser.michael@gmail.com>
2011-10-17 17:43:42 +0200

    add rubymine as gem editor

--- bash/alias.sh
+++ bash/alias.sh
@@ -36,6 +36,7 @@ alias bo="EDITOR=mine bundle open"
 alias nob-ruby="`which ruby`" # ruby without bundler
 alias rc="rvm use system && redcar ."
 alias rvmrake="rvm ree,1.9.2 exec rake" # run rake tasks with 1.8.7 and 1.9.2
+alias ge="EDITOR=mine gem edit"
 
 #GEMS
 alias gemi='sudo gem install --no-rdoc --no-ri'
