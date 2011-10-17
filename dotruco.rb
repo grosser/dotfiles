4aeb07b763ea0e4bab47669160900303c9711fa0
grosser <grosser.michael@gmail.com>
2011-10-17 17:45:51 +0200

    play with ruco themes

--- dotruco.rb
+++ dotruco.rb
@@ -4,4 +4,11 @@ Ruco.configure do
   options.editor_remove_trailing_whitespace_on_save = true
   options.editor_blank_line_before_eof_on_save = true
   # options.editor_line_numbers = true
+  # options.color_theme = "https://raw.github.com/ChrisKempson/Tomorrow-Theme/master/TextMate/Tomorrow-Night-Bright.tmTheme"
+  # options.color_theme = "https://raw.github.com/ryanb/textmate-themes/master/railscasts.tmTheme"
+  # options.color_theme = "https://raw.github.com/altercation/solarized/master/textmate-colors-solarized/Solarized%20%28dark%29.tmTheme"
+  #  options.color_theme = "https://raw.github.com/altercation/solarized/master/textmate-colors-solarized/Solarized%20%28light%29.tmTheme"
+    options.color_theme = "https://raw.github.com/deplorableword/textmate-solarized/master/Solarized%20%28dark%29.tmTheme"
+
+
 end
