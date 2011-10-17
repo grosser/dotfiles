640c6d8223de3617df59ba591463a9f23ae77716
grosser <grosser.michael@gmail.com>
2011-10-17 17:48:51 +0200

    try rbenv

--- bash/env.sh
+++ bash/env.sh
@@ -1,6 +1,7 @@
-export EDITOR="nano"
+export EDITOR="ruco"
 export PATH="~/bin:$PATH"
-export JDK_HOME="/usr/lib/jvm/java-6-sun/jre"
+export JAVA_HOME="/usr/lib/jvm/java-6-openjdk"
+export RUBYMINE_JDK="/usr/lib/jvm/java-6-sun/jre"
 export GEM_OPEN_EDITOR=redcar
 RBXOPT="-Xrbc.db" # do not clutter up everything with .rbc files when using rubinius
 
@@ -10,11 +11,15 @@ export HISTCONTROL=ignoredups
 # ... and ignore same sucessive entries.
 export HISTCONTROL=ignoreboth
 
+
+export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/.rbenv/versions/1.9.3-rc1/bin:$PATH"
+
 # Load RVM if it is installed,
 #  first try to load  user install
 #  then try to load root install, if user install is not there.
-if [ -s "$HOME/.rvm/scripts/rvm" ] ; then
-  . "$HOME/.rvm/scripts/rvm"
-elif [ -s "/usr/local/rvm/scripts/rvm" ] ; then
-  . "/usr/local/rvm/scripts/rvm"
-fi
+#if [ -s "$HOME/.rvm/scripts/rvm" ] ; then
+#  . "$HOME/.rvm/scripts/rvm"
+#elif [ -s "/usr/local/rvm/scripts/rvm" ] ; then
+#  . "/usr/local/rvm/scripts/rvm"
+#fi
