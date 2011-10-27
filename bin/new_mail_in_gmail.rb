373c91457ed0eb81d55c708de837730fcb7f1e4f
grosser <grosser.michael@gmail.com>
2011-10-27 12:05:26 -0700

    no longer ignore non-secret bin

---
+++ bin/new_mail_in_gmail.rb
@@ -0,0 +1,7 @@
+#!//usr/bin/env ruby
+# Open mailto in gmail.
+# Got to System > Preferences > Preferred Applications
+# and add '/home/your-name/bin/new_mail_in_gmail.rb %s'
+mail = ARGV[0].sub('mailto://','')
+puts "sending to #{mail}"
+exec "chromium-browser 'https://mail.google.com/mail?view=cm&tf=0&to=#{mail}'"
