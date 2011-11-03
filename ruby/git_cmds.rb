c281354236efaecc0fea220706b3dd105b508e96
grosser <grosser.michael@gmail.com>
2011-11-03 00:09:49 -0700

    use simpler version of cim and ciam

--- ruby/git_cmds.rb
+++
@@ -1,5 +0,0 @@
-# can take advantage of ARGV, which git aliases cannot
-# check in all changes with a message tat does not need quotes around it <-> lazy
-def args_to_string(cmd)
-  `#{cmd} "#{ARGV.join(' ').gsub('"','`')}"`
-end
