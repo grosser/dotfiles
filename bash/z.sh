b60d369661b0f7e9cd91bea0311154d60e15bf2d
grosser <grosser.michael@gmail.com>
2012-01-13 20:35:32 -0800

    make git_promt exit status work by removing parts of z

--- bash/z.sh
+++ bash/z.sh
@@ -163,18 +163,19 @@ z() {
  fi
 }
 
-if complete &> /dev/null; then
-  # bash tab completion
-  complete -C 'z --complete "$COMP_LINE"' z
-  # populate directory list. avoid clobbering other PROMPT_COMMANDs.
-  echo $PROMPT_COMMAND | grep -q "z --add"
-  [ $? -gt 0 ] && PROMPT_COMMAND='z --add "$(pwd -P 2>/dev/null)";'"$PROMPT_COMMAND"
-elif compctl &> /dev/null; then
-  # zsh tab completion
-  _z_zsh_tab_completion() {
-    local compl
-    read -l compl
-    reply=(`z --complete "$compl"`)
-  }
-  compctl -U -K _z_zsh_tab_completion z
-fi
+# if this is enabled last exit status git_promt highlighting does not longer work
+#if complete &> /dev/null; then
+#  # bash tab completion
+#  complete -C 'z --complete "$COMP_LINE"' z
+#  # populate directory list. avoid clobbering other PROMPT_COMMANDs.
+#  echo $PROMPT_COMMAND | grep -q "z --add"
+#  [ $? -gt 0 ] && PROMPT_COMMAND='z --add "$(pwd -P 2>/dev/null)";'"$PROMPT_COMMAND"
+#elif compctl &> /dev/null; then
+#  # zsh tab completion
+#  _z_zsh_tab_completion() {
+#    local compl
+#    read -l compl
+#    reply=(`z --complete "$compl"`)
+#  }
+#  compctl -U -K _z_zsh_tab_completion z
+#fi
