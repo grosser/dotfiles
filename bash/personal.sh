0a03634ddfaa2047d5c147050748eff20f140d48
grosser <grosser.michael@gmail.com>
2012-05-02 20:06:52 -0700

    GC settings for faster app/test boot

--- bash/personal.sh
+++ bash/personal.sh
@@ -1,4 +1,3 @@
-
 #Stuff that no-one else need, add and modify here to keep forks clean
 alias kj="pkill -f java"
 alias kf="pkill -f firefox"
@@ -22,6 +21,11 @@ function pdf-decrypt(){
 export GEMEDITOR='mine'
 
 # ZenDesk specific
+export RUBY_HEAP_MIN_SLOTS=300000
+export RUBY_HEAP_SLOTS_INCREMENT=250000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+export RUBY_GC_MALLOC_LIMIT=50000000
+
 function zd {
   ~/.zendesk/"${1}" "${2}"
 }
