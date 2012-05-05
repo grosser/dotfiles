3f19fdda160090917aa753f7f223d32589c9ddc9
grosser <grosser.michael@gmail.com>
2012-05-05 08:16:35 -0700

    some nice colors for ls on mac

--- bash/env.sh
+++ bash/env.sh
@@ -3,6 +3,7 @@ export PATH="~/dotfiles/bin:~/dotfiles/secret/bin:/usr/local/bin:$PATH"
 export JAVA_HOME="/usr/lib/jvm/java-6-openjdk"
 export RUBYMINE_JDK="/usr/lib/jvm/java-6-sun/jre"
 export GEM_OPEN_EDITOR=redcar
+export CLICOLOR=1 # colors for e.g. ls on mac
 RBXOPT="-Xrbc.db" # do not clutter up everything with .rbc files when using rubinius
 
 ### HISTORY
