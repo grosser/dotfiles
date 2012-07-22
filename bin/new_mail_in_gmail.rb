5f54641eb5dcd3fd0b3f10b4acf02b19e5d2719c
grosser <grosser.michael@gmail.com>
2012-07-21 18:38:35 -0700

    few more comments

--- bin/new_mail_in_gmail.rb
+++ bin/new_mail_in_gmail.rb
@@ -1,5 +1,5 @@
 #!//usr/bin/env ruby
-# Open mailto in gmail.
+# Open mailto in gmail for ubuntu.
 # Got to System > Preferences > Preferred Applications
 # and add '/home/your-name/bin/new_mail_in_gmail.rb %s'
 mail = ARGV[0].sub('mailto://','')
