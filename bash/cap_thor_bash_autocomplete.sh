7768a576a3e0fef3c56a1c756431ae5bbe6108ab
grosser <grosser.michael@gmail.com>
2012-07-21 20:00:51 -0700

    rake autocomplete with cache, so much faster :D

--- bash/cap_thor_bash_autocomplete.sh
+++ bash/cap_thor_bash_autocomplete.sh
@@ -1,9 +1,6 @@
 export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
 
-_rakecomplete() {
-  COMPREPLY=($(compgen -W "`rake -s -T 2>/dev/null | awk '{{print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
-  return 0
-}
+
 
 _capcomplete() {
   COMPREPLY=($(compgen -W "`cap  -T  2>/dev/null| awk '{{ if ( $3 ~ /\#/ ) print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
@@ -15,6 +12,6 @@ _thorcomplete() {
   return 0
 }
 
-complete -o default -o nospace -F _rakecomplete rake
+
 complete -o default -o nospace -F _capcomplete cap
 complete -o default -o nospace -F _thorcomplete thor
