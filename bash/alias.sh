b6341c3dcca3a47e47b293bae3d4d9a0bbefec85
grosser <grosser.michael@gmail.com>
2012-08-12 17:28:26 -0700

    lil cleanup

--- bash/alias.sh
+++ bash/alias.sh
@@ -26,25 +26,12 @@ alias svn_rm_new="svn st | egrep '^\?[ ]+' | sed -e 's|^?[ ]*||' | xargs -i rm '
 alias svn_rm_missing="svn st | egrep '^\![ ]+' | sed -e 's|^![ ]*||' | xargs -i svn rm '{}'"
 
 #GIT
-alias work='git co work'
 alias master='git co master'
-alias deploy='git co deploy'
 
 #Rails
 alias ttr="touch tmp/restart.txt"
-alias ss="script/server"
-alias sc="script/console"
-alias sg="script/generate"
 
 # RUBY
-alias sa="autospec -f" #run autospec without the initial full run
 alias be="bundle exec"
-alias bi="bundle install"
 alias b="bundle check 2>&1 > /dev/null || { bundle --quiet || bundle check > /dev/null; } && bundle exec"
 alias bo="EDITOR=mine bundle open"
-alias nob-ruby="`which ruby`" # ruby without bundler
-alias ge="EDITOR=mine gem edit"
-
-#GEMS
-alias gemi='sudo gem install --no-rdoc --no-ri'
-alias gemu='sudo gem uninstall'
--- bash/personal.sh
+++ bash/personal.sh
@@ -1,10 +1,9 @@
 #Stuff that no-one else need, add and modify here to keep forks clean
 alias kj="pkill -f java"
 alias kf="pkill -f firefox"
-alias da="cd /apps/dawanda/main"
-alias rs="cd /apps/rs"
 alias m="mine . > /dev/null 2>&1 &"
-alias wrake="`which rake` _0.8.7_"
+alias wrake="`which rake`" # rake without bundler
+alias wruby="`which ruby`" # ruby without bundler
 alias pryr="pry -r ./config/environment -r rails/console/app -r rails/console/helpers" # pry as rails console, without pry as dependency
 alias r="rvm 1.9.3 exec ruco"
 alias skype-restart="killall -9 skype && video-skype"
@@ -21,8 +20,6 @@ function take(){
 function pdf-decrypt(){
   qpdf --decrypt $1 $1.tmp && mv -f $1.tmp $1
 }
-
-export GEMEDITOR='mine'
 
 # ZenDesk specific
 export RUBY_HEAP_MIN_SLOTS=300000
