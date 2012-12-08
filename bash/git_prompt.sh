47fcf3326685d0ad70eb03094e3e6eb9392d0301
grosser <grosser.michael@gmail.com>
2012-12-08 14:14:16 -0800

    lightning looks bad on mountain lion prompt

--- bash/git_prompt.sh
+++ bash/git_prompt.sh
@@ -19,7 +19,7 @@ function parse_git_branch {
   diverge_pattern="# Your branch and (.*) have diverged"
 
   if [[ ! ${git_status}} =~ "working directory clean" ]]; then
-state="${RED}⚡"
+state="${RED}☃"
   fi
   # add an else if or two here if you want to get more specific
   if [[ ${git_status} =~ ${remote_pattern} ]]; then
