dfdeabdd405bdf687b85a3fac34fd8ffe45c7f8b
Michael Grosser <mgrosser@admin01.sys.zendesk.com>
2013-07-30 01:31:04 +0000

    different config for remote hosts

--- bash/personal.sh
+++ bash/personal.sh
@@ -33,5 +33,3 @@ export RUBY_HEAP_SLOTS_INCREMENT=1000000
 export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_HEAP_FREE_MIN=500000
-
-PATH="/Applications/eclipse-adt/sdk/tools:/Applications/eclipse-adt/sdk/platform-tools/:$PATH"
