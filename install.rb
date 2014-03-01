b5f25195547be0637d38c0dae8f767d9cb81e889
grosser <michael@grosser.it>
2014-03-01 10:48:41 -0800

    simplify installer and make it more robust

--- install.rb
+++ install.rb
@@ -1,11 +1,10 @@
 #!/usr/bin/env ruby
 # backup files and replace
 
-def backup_and_replace(dotfile, home, backup)
+def backup_and_replace(dotfile, backup)
   # dotxxx -> home/.xxx
   if File.basename(dotfile) =~ /^dot/
-    file = File.basename(dotfile).sub('dot','.')
-    original = "#{home}/#{file}"
+    original = File.basename(dotfile).sub('dot','.')
 
   # translate magic comment in first lines
   elsif File.file?(dotfile) and File.readlines(dotfile)[0..1].join("\n") =~ %r{^(//|#)\->([/a-z\d\.]+)}
@@ -17,27 +16,28 @@ def backup_and_replace(dotfile, home, backup)
 
   puts "linking #{dotfile} -> #{original}"
 
-  prefix = move_file_to_backup(original, backup)
-  sh "#{prefix} ln -s #{dotfile} #{original}"
+  move_file_to_backup(original, backup)
+  sh "#{maybe_sudo(original)}ln -s #{dotfile} #{original}"
+end
+
+def maybe_sudo(file)
+  'sudo ' if File.exist?(file) && !system("touch #{file}")
 end
 
 def move_file_to_backup(original, backup)
-  prefix = 'sudo' unless system("touch #{original}")
-  if File.exist?(original)
-    sh "#{prefix} mv #{original} #{backup}/#{original.gsub('/', '-')} 2>&1"
-  end
-  prefix
+  return unless File.exist?(original)
+  sh "#{maybe_sudo(original)}mv #{original} #{backup}/#{original.gsub('/', '-')}"
 end
 
-def add_include_to_gitconfig(home, dotfiles)
-  gitconfig = "#{home}/.gitconfig"
+def add_include_to_gitconfig(dotfiles)
+  gitconfig = ".gitconfig"
   if File.exist?(gitconfig) && !File.read(gitconfig).include?("[include]")
     File.open(gitconfig, "a+") { |f| f.write "\n\n[include]\n  path=#{dotfiles}/gitconfig\n" }
   end
 end
 
-def create_bckup_folder(home)
-  backup = "#{home}/backup_dotfiles_#{Time.now.strftime('%Y-%m-%dT%H:%M:%S')}"
+def create_backup_folder
+  backup = "backup_dotfiles_#{Time.now.strftime('%Y-%m-%dT%H:%M:%S')}"
   sh "mkdir #{backup}"
   backup
 end
@@ -49,19 +49,19 @@ def sh(command)
 end
 
 dotfiles = File.expand_path(File.dirname(__FILE__))
-home = File.expand_path('~')
+raise "You must be in your home folder" unless Dir.pwd == File.expand_path('~')
 
-expected = "#{home}/dotfiles"
-raise "dotfiles must be checked out as #{expected}" if expected != dotfiles
+expected = File.expand_path("~/dotfiles")
+raise "dotfiles must be checked out as #{expected}" unless dotfiles == expected
 
-backup = create_bckup_folder(home)
+backup = create_backup_folder
 
 # backup and replace files through links
 Dir["#{dotfiles}/*", "#{dotfiles}/secret/*"].each do |dotfile|
-  backup_and_replace(dotfile, home, backup)
+  backup_and_replace(dotfile, backup)
 end
 
-move_file_to_backup("#{home}/.zshrc", backup)
-add_include_to_gitconfig(home, dotfiles)
+move_file_to_backup(".zshrc", backup)
+add_include_to_gitconfig(dotfiles)
 
 puts "Everything done!!"
