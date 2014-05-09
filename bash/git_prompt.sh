63848fda9ed40ec5ab8d4afa36f43cfa0d81dd9a
grosser <michael@grosser.it>
2014-05-09 09:50:43 -0700

    git update

--- bash/git_prompt.sh
+++ bash/git_prompt.sh
@@ -14,26 +14,26 @@ function parse_git_branch {
 
   git rev-parse --git-dir &> /dev/null
   git_status="$(git status 2> /dev/null)"
-  branch_pattern="^# On branch ([^${IFS}]*)"
-  remote_pattern="# Your branch is ([a-z]+)"
-  diverge_pattern="# Your branch and (.*) have diverged"
+  branch_pattern="^On branch ([^${IFS}]*)"
+  remote_pattern="Your branch is (behind|ahead)"
+  diverge_pattern="Your branch and (.*) have diverged"
 
   if [[ ! ${git_status}} =~ "working directory clean" ]]; then
-state="${RED}☃"
+    state="${RED}☃"
   fi
   # add an else if or two here if you want to get more specific
   if [[ ${git_status} =~ ${remote_pattern} ]]; then
-if [[ ${BASH_REMATCH[1]} == "ahead" ]]; then
-remote="${YELLOW}↑"
+    if [[ ${BASH_REMATCH[1]} == "ahead" ]]; then
+      remote="${YELLOW}↑"
     else
-remote="${YELLOW}↓"
+      remote="${YELLOW}↓"
     fi
-fi
-if [[ ${git_status} =~ ${diverge_pattern} ]]; then
-remote="${YELLOW}↕"
   fi
-if [[ ${git_status} =~ ${branch_pattern} ]]; then
-branch=${BASH_REMATCH[1]}
+  if [[ ${git_status} =~ ${diverge_pattern} ]]; then
+    remote="${YELLOW}↕"
+  fi
+  if [[ ${git_status} =~ ${branch_pattern} ]]; then
+    branch=${BASH_REMATCH[1]}
     echo " (${branch})${remote}${state}"
   fi
 }
