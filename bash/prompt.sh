0b642f4244e8e9407aa3f68ba9b7b3b3aa58b696
Michael Grosser <michael@grosser.it>
2016-12-04 21:49:14 -0800

    .local makes webrick server lookups slow ... so pick sth else
    https://bugs.ruby-lang.org/issues/13007

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -48,7 +48,7 @@ function prompt_func() {
 
   # show host on remote servers
   host=$(hostname)
-  if [[ $host != *".local"* ]]; then
+  if [[ $host != *"mgrosser"* ]]; then
     host="${host} - "
   else
     host=""
