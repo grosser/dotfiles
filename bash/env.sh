15607e25385019d7f5dfd9ffc870310eeb8cf570
Michael Grosser <michael@grosser.it>
2019-06-07 19:38:23 -0700

    improve history

--- bash/env.sh
+++ bash/env.sh
@@ -3,18 +3,20 @@ export PATH="$HOME/dotfiles/bin:$HOME/dotfiles/secret/bin:$HOME/.nodenv/shims:/u
 if [ -f /usr/libexec/java_home ]; then
   export JAVA_HOME=$(/usr/libexec/java_home)
 fi
-#export RUBYMINE_JDK="/usr/lib/jvm/java-6-sun/jre"
 export CLICOLOR=1 # colors for e.g. ls on mac
 export RBXOPT="-Xrbc.db" # do not clutter up everything with .rbc files when using rubinius
 
-### HISTORY
-export HISTSIZE=1000000 #size of history
-export HISTCONTROL=ignorespace:ignoredups # ignore dulicates, cmds beginning with space
+# HISTORY https://sanctum.geek.nz/arabesque/better-bash-history
+export HISTSIZE=1000000 # size of history
+export HISTCONTROL=ignorespace:ignoredups # ignore duplicates, cmds beginning with space
+export HISTIGNORE='ls:bg:fg:history' # ignore useless commands
+export HISTTIMEFORMAT='%F %T ' # show when commands were executed
 
-### Ignore mail
+
+# Ignore mail
 unset -v MAIL;
 unset -v MAIL_WARNING;
 
-#in case-sensitive autocompletion
+# in case-sensitive autocompletion
 tty -s && bind 'set completion-ignore-case on' 2>/dev/null
 
