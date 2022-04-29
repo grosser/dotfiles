3cf0b7fed9480c6bb76404027986bc11cde40d1d
Michael Grosser <michael@grosser.it>
2022-04-29 15:27:24 -0700

    sort version managers

--- bash/personal.sh
+++ bash/personal.sh
@@ -41,23 +41,3 @@ function kbadpdb(){
 
 export BUNDLER_EDITOR=mine
 export GEM_EDITOR=mine
-
-# Big app GC tuning
-export RUBY_HEAP_MIN_SLOTS=1000000 # ruby < 2.1
-export RUBY_GC_HEAP_INIT_SLOTS=1000000 # ruby 2.1+
-export RUBY_HEAP_SLOTS_INCREMENT=1000000
-export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-export RUBY_GC_MALLOC_LIMIT=1000000000
-export RUBY_HEAP_FREE_MIN=500000
-
-# GO
-# PATH="$HOME/.goenv/shims:$PATH"
-PATH="$PATH:$HOME/go/bin"
-
-# Rust
-PATH="$HOME/.cargo/bin:$PATH"
-
-# make ruby installs fast by reusing openssl
-# see `brew list openssl`
-export RUBY_CONFIGURE_OPTS=--with-openssl-dir=/opt/homebrew/Cellar/openssl@3/3.0.2
-
---
+++ bash/ruby.sh
@@ -0,0 +1,21 @@
+# chruby or rbenv
+if [ -f /opt/homebrew/opt/chruby/share/chruby/chruby.sh ]; then
+  source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
+  RUBIES+=(~/.rbenv/versions/*)
+  chruby 3.1.0 # pick a default ruby
+  source /opt/homebrew/opt/chruby/share/chruby/auto.sh
+elif command -v rbenv >/dev/null 2>&1; then
+  eval "$(rbenv init -)"
+fi
+
+# Big app GC tuning
+export RUBY_HEAP_MIN_SLOTS=1000000 # ruby < 2.1
+export RUBY_GC_HEAP_INIT_SLOTS=1000000 # ruby 2.1+
+export RUBY_HEAP_SLOTS_INCREMENT=1000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+export RUBY_GC_MALLOC_LIMIT=1000000000
+export RUBY_HEAP_FREE_MIN=500000
+
+# make ruby installs fast by reusing openssl
+# see `brew list openssl`
+export RUBY_CONFIGURE_OPTS=--with-openssl-dir=/opt/homebrew/Cellar/openssl@3/3.0.2
