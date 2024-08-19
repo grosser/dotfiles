de506a548d26e23ebdcd62844f34e649ca178dbd
Michael Grosser <michael@grosser.it>
2024-08-19 11:19:05 -0700

    okta auth check

--- bin/okta_auth_valid.rb
+++ bin/okta_auth_valid.rb
@@ -1,9 +1,10 @@
+#!/usr/bin/ruby
 require "base64"
 require "json"
 
 config = JSON.load(File.read(File.expand_path("~/.kube/pkce_helper.json")))
 config.each do |id, data|
   payload = data.fetch("id_token").split(".", 3)[1]
-  remaining = Time.now - Time.at(JSON.parse(Base64.urlsafe_decode64(payload)).fetch("exp"))
+  remaining = Time.at(p(JSON.parse(Base64.urlsafe_decode64(payload))).fetch("exp")) - Time.now
   puts "#{id}\t-> #{remaining.to_i}s"
 end
