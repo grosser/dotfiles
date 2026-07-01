aa90a7aace8af51988f7f6cafa88ce389f6a5fbd
Michael Grosser <michael@grosser.it>
2026-07-01 13:55:49 -0700

    kdelf as plugin

--- bash/personal.sh
+++ bash/personal.sh
@@ -25,12 +25,6 @@ alias clc="claude --continue"
 function k() { if [[ $2 = -* ]]; then kubectl --context "$1" "${@:2}"; else kubectl "$2" --context "$1" "${@:3}"; fi; }
 function ka() { if [[ $2 = -* ]]; then kubectl --as admin --as-group system:masters --context "$1" "${@:2}"; else kubectl "$2" --as admin --as-group system:masters --context "$1" "${@:3}"; fi; }
 
-# delete without finalizers (does not support -l, for that use https://github.com/grosser/kubectl-patch-all)
-function kdelf() {
-  kubectl patch --as admin --as-group system:masters --patch '{"metadata":{"finalizers":null}}' --type merge --context "$@" && \
-  kubectl delete --as admin --as-group system:masters  --context "$@"
-}
-
 # ns can be stuck with spec.finalizers and a regular patch can't remove them
 function kdelns() {
   kubectl get --as admin --as-group system:masters --context "$1" namespace "$2" -ojson | jq '.spec.finalizers = []' | kubectl replace --as admin --as-group system:masters --context "$1" --raw "/api/v1/namespaces/$2/finalize" -f -
